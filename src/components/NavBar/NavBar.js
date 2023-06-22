fe19661832f6862115ca70f22cd7e9a4f064d110
randika0514 <randikadimantha@gmail.com>
2023-06-22 13:40:57 +0530

    Updated Navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './navbarStyle.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function NavBar() {
     return (
         <div>
             <nav>
+                <h2>RD</h2>
                 <ul>
                     <li>About Me</li>
                     <li>Services</li>
@@ -14,6 +17,10 @@ function NavBar() {
                     <li>Activities</li>
                     <li>Contact Me</li>
                 </ul>
+
+                <div className='toggle-btn'>
+                    <FontAwesomeIcon icon={faBars} bounce />
+                </div>
             </nav>
         </div>
     )
