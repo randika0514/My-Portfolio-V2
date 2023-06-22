ce46695e37a2fdba2bf79633cec6a919f0d47b66
randika0514 <randikadimantha@gmail.com>
2023-06-22 12:13:01 +0530

    Added Navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './navbarStyle.css'
+
+function NavBar() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>About Me</li>
+                    <li>Services</li>
+                    <li>Education</li>
+                    <li>Skills</li>
+                    <li>Projects</li>
+                    <li>Activities</li>
+                    <li>Contact Me</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
